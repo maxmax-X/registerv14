a5ab40ce1884b4859ac95fb27b4615ccfe92fd7a
maxmax-X <maxmaxdev@protonmail.com>
2025-07-03 08:55:25 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,37 +1,37 @@
 module.exports = {
     // 🔹 Genel Sunucu Ayarları
-    sunucuID: "1388061179909242902",
+    sunucuID: "",
     tag: ["Test - "], // Array formatında tag sistemi
     
     // 🔹 Kayıt Sistemi
-    registerChat: "1388061259768791120",
-    registerYetkili: "1384829536746864725",
-    kayıtsızRol: "1388062281006452816",
+    registerChat: "",
+    registerYetkili: "",
+    kayıtsızRol: "",
     kayıtsızİsim: "· İsim | ",
 
     // 🔹 Cinsiyet Rolleri
-    üyeRol: "1388062126802993182", // Genel üye rolü
-    erkekRolleri: ["1389937756851142739", "1388083427164684309"], // Üye rolü + Erkek rolü
-    kadınRolleri: ["1388083469074042910", "1389937608674771017"], // Üye rolü + Kadın rolü
+    üyeRol: "", // Genel üye rolü
+    erkekRolleri: ["", ""], // Üye rolü + Erkek rolü
+    kadınRolleri: ["", ""], // Üye rolü + Kadın rolü
 
     // 🔹 Tag Sistemi
-    tagRol: "1389855544562552882",
-    tagLog: "1389855743695392860",
+    tagRol: "",
+    tagLog: "",
 
     // 🔹 Genel Kanallar
-    genelChat: "1388061181079719936",
-    updateChannel: "1388117409759756288", // Güncelleme mesajları için kanal
-    boosterRol: "1388062126802993182",
+    genelChat: "",
+    updateChannel: "", // Güncelleme mesajları için kanal
+    boosterRol: "",
     
     // 🔹 Yetkili Rolleri
-    adminRolleri: ["1384829536746864725"], // Admin rolleri
+    adminRolleri: [""], // Admin rolleri
     moderatorRolleri: [], // Moderatör rolleri
     
     // 🔹 Log Kanalları
-    welcomeChannel: "1388061259768791120", // Hoş geldin kanalı
-    leaveChannel: "1389884181168787496", // Çıkış kanalı
-    modlogChannel: "1389884232330903663", // Moderasyon log kanalı
-    voiceLogChannel: "1388061181079719938", // Ses kanalı log
+    welcomeChannel: "", // Hoş geldin kanalı
+    leaveChannel: "", // Çıkış kanalı
+    modlogChannel: "", // Moderasyon log kanalı
+    voiceLogChannel: "", // Ses kanalı log
     
     // 🔹 Otomatik Rol Sistemi
     otoRol: [], // Otomatik verilecek roller
@@ -41,14 +41,14 @@ module.exports = {
     reklamEngel: true, // Reklam engelleme sistemi
     
     // 🔹 Invite Sistemi
-    inviteChannel: "1389887820172234762", // Invite log kanalı
+    inviteChannel: "", // Invite log kanalı
     
     // 🔹 Yaş Sistemi
     minYas: 13, // Minimum yaş sınırı
     maxYas: 99, // Maximum yaş sınırı
     
     // 🔹 Stat Sistemi
-    statChannel: "1389887942394380360", // İstatistik kanalı
+    statChannel: "", // İstatistik kanalı
     
     // 🔹 Ceza Sistemi
     cezaLog: "", // Ceza log kanalı
@@ -56,8 +56,8 @@ module.exports = {
     muteRol: "", // Mute rolü
     
     // 🔹 Özel Ayarlar
-    botSahipleri: ["1384829536746864725"], // Bot sahipleri
-    geliştiriciler: ["1384829536746864725"], // Geliştiriciler
+    botSahipleri: [""], // Bot sahipleri
+    geliştiriciler: [""], // Geliştiriciler
     
     // 🔹 Emoji Ayarları - Default emojiler
     emojiler: {
